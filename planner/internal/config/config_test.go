6c8006a5425e55fca2e9c3e90b5631157975aa37
Andrey Kalistratov <kalistratov.aa@phystech.edu>
2026-03-24 09:44:39 +0300

    planner: add config load tests

--- planner/internal/config/config_test.go
+++ planner/internal/config/config_test.go
@@ -2,37 +2,70 @@ package config
 
 import (
 	"errors"
-	"log/slog"
+	"os"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
 )
 
-func TestSimpleConfig(t *testing.T) {
-	cfg, err := Load("testdata/simple.json")
-	if err != nil {
-		t.Fatal(err)
+func writeFile(t *testing.T, config string) (path string, cleanup func()) {
+	f, err := os.CreateTemp("", "tm-*")
+	require.NoError(t, err)
+
+	_, err = f.WriteString(config)
+	require.NoError(t, err)
+
+	require.NoError(t, f.Close())
+
+	cleanup = func() {
+		require.NoError(t, os.Remove(f.Name()))
 	}
+	return f.Name(), cleanup
+}
+
+func testWithStr(t *testing.T, config string) (*Config, error) {
+	cfgPath, cleanup := writeFile(t, config)
+	defer cleanup()
+
+	return Load(cfgPath)
+}
+
+func TestSimpleConfig(t *testing.T) {
+	cfgStr := `[log]
+	level = "debug"
+	file = "/var/log/task-manager/planner.log"
+	`
+
+	cfg, err := testWithStr(t, cfgStr)
+	require.NoError(t, err)
 
 	expected := &Config{
-		Logging: LogConfig{
-			Level: slog.LevelDebug,
+		Log: Log{
+			Level: "debug",
 			File:  "/var/log/task-manager/planner.log",
 		},
 	}
-	if diff := cmp.Diff(expected, cfg); diff != "" {
-		t.Errorf("Load() mismatch (-want +got):\n%s", diff)
-	}
+	require.Equal(t, expected, cfg)
 }
 
 func TestCorruptedConfig(t *testing.T) {
-	if _, err := Load("testdata/corrupted.json"); !errors.Is(err, ErrLoad) {
+	cfgStr := `[log
+	level = "warn"
+	file = "/var/log/task-manager/planner.log"
+	`
+
+	if _, err := testWithStr(t, cfgStr); !errors.Is(err, ErrLoad) {
 		t.Error("expected ErrLoad")
 	}
 }
 
 func TestInvalidLogLevel(t *testing.T) {
-	if _, err := Load("testdata/invalid_log_level.json"); !errors.Is(err, ErrLoad) {
-		t.Error("expected ErrValidate")
+	cfgStr := `[log]
+	level = "warning"
+	file = "/var/log/task-manager/planner.log"
+	`
+
+	if _, err := testWithStr(t, cfgStr); !errors.Is(err, ErrValidate) {
+		t.Error("expected ErrLoad")
 	}
 }
