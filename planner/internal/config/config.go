0c14d5cff6454c50e85601e11a7d512010d4db3a
Andrey Kalistratov <kalistratov.aa@phystech.edu>
2026-03-30 16:33:31 +0300

    planner: update config

--- planner/internal/config/config.go
+++ planner/internal/config/config.go
@@ -1,45 +1,82 @@
 package config
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log/slog"
 	"os"
+)
 
-	"github.com/BurntSushi/toml"
-	"github.com/creasty/defaults"
-	"github.com/go-playground/validator"
+const UnixSocket = "/var/run/planner.sock"
+
+type (
+	// Config represents the application configuration.
+	Config struct {
+		Logging LogConfig `json:"logging"`
+		DB      DBConfig  `json:"db"`
+	}
+
+	// LogConfig contains logging configuration for the application.
+	LogConfig struct {
+		// Level sets the logging level. Possible values: debug, info, warn, error.
+		Level slog.Level `json:"level"`
+
+		// File specifies the path to the log file.
+		File string `json:"file"`
+	}
+
+	// DBConfig contains database configuration for the application.
+	DBConfig struct {
+		// File specifies the path to the database file.
+		File string `json:"file"`
+	}
+)
+
+var (
+	// ErrFind indicates that config was not found neither by the given path nor by standard ones.
+	ErrFind = errors.New("failed to find config")
+
+	// ErrLoad indicates that loading or parsing the config file failed.
+	ErrLoad = errors.New("failed to load config")
 )
 
 // Load looks for a config file first by the given path, then by standard paths.
 //
 // Pass "" if no path is provided.
 func Load(path string) (*Config, error) {
-	var cfg Config
-	if err := defaults.Set(&cfg); err != nil {
-		return nil, fmt.Errorf("%w: %v", ErrInvalidDefaults, err)
-	}
+	cfg := newDefault()
 
-	if p := find(path); p != "" {
-		if _, err := toml.DecodeFile(p, &cfg); err != nil {
-			return nil, fmt.Errorf("%w: %v", ErrLoad, err)
-		}
-	}
-
-	if err := validator.New().Struct(&cfg); err != nil {
-		return nil, fmt.Errorf("%w: %v", ErrValidate, err)
-	}
-	return &cfg, nil
-}
-
-func find(path string) string {
 	paths := []string{
 		path,
-		"/etc/task-manager/planner/config.toml",
-		"./planner/config/config.toml",
+		"/etc/task-manager/planner/config.json",
+		"./planner/internal/config/testdata/simple.json",
 	}
-	for _, p := range paths {
-		if _, err := os.Stat(p); err == nil {
-			return p
+
+	for _, path := range paths {
+		data, err := os.ReadFile(path)
+		if errors.Is(err, os.ErrNotExist) {
+			continue
 		}
+		if err != nil {
+			return nil, fmt.Errorf("%w: %v", ErrLoad, err)
+		}
+		if err := json.Unmarshal(data, cfg); err != nil {
+			return nil, fmt.Errorf("%w: %v", ErrLoad, err)
+		}
+		return cfg, nil
 	}
-	return ""
+	return nil, ErrFind
+}
+
+func newDefault() *Config {
+	return &Config{
+		Logging: LogConfig{
+			Level: slog.LevelError,
+			File:  "/var/log/task-manager/planner.log",
+		},
+		DB: DBConfig{
+			File: "/var/lib/task-manager/planner.db",
+		},
+	}
 }
