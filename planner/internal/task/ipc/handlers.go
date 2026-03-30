5b2bc811bfa57204355033eb9f74486e30b30cb6
Andrey Kalistratov <kalistratov.aa@phystech.edu>
2026-03-30 16:36:09 +0300

    planner: skeleton task

--- planner/internal/task/ipc/handlers.go
+++ planner/internal/task/ipc/handlers.go
@@ -1,16 +1,16 @@
 package ipc
 
 import (
-	"github.com/andrey-kalistratov/task-manager/planner/internal/task"
-	"github.com/andrey-kalistratov/task-manager/planner/unixsocket"
+	"task-manager/planner/internal/task"
+	"task-manager/planner/unixsocket"
 )
 
 type AddHandler struct {
-	service *task.Service
+	storage task.Storage
 }
 
-func NewAddHandler(service *task.Service) *AddHandler {
-	return &AddHandler{service: service}
+func NewAddHandler(storage task.Storage) *AddHandler {
+	return &AddHandler{storage: storage}
 }
 
 func (h *AddHandler) ServeIPC(request unixsocket.Request) unixsocket.Response {
